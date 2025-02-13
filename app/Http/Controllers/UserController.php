bf4eb24dc84976596ca468d49392a099459603ca
DESKTOP-7VUTVPT\Emilio <1448592@alu.murciaeduca.es>
2025-02-13 17:49:07 +0100

    EJERCICIO 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Course;
+use App\Models\User;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UserController extends Controller
 {
+    public function index()
+    {
+        $user = User::all();
+        return view('user.index');
+    }
 
-
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'role' => 'required',
+        ]);
+        $user = User::find($id);
+        $user->update($request->all());
+        return redirect()->route('user.index')
+            ->with('User updated successfully');
+    }
 }
